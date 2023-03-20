bfbc5ad2284a0d18b1e95adfbd8910cc6c4249fb
Andy <andrew.wells@hotmail.co.uk>
2023-03-20 21:14:00 +0000

    initial commit

---
+++ footer.php
@@ -0,0 +1,52 @@
+<div id="footer_bottom_bar">   
+            <p>Company Number:0805 7707</p>
+            <p> VAT Number:160272829</p>
+        </div>
+
+<div class="divider"></div>
+
+    <div id="footer">
+        
+        <div id="footer_columns">
+
+            <div class="footer_column" id="site_map">
+                <ul> 
+                <li>Site Map</li><br>
+                <?php echo $link_stack.$footer_only_links;?>
+                </ul>
+
+                <div id="mobile_padding">
+                </div>
+            </div>
+
+            <div class="footer_column" id="contact_bar">
+                <br>
+                    <ul>
+                        <li><a href="https://www.facebook.com/osborneplastering.co.uk/" target="_blank"><img src="images/facebook.png"/></a>
+                        </li>
+                        <li><a href="tel:080095577070"><img src="images/phone.png"/></a>
+                        </li>
+                        <li><a href="mailto:contact@osborneplastering.co.uk"><img src="images/email.png"/></a>
+                        </li>
+                        <li><a href="https://twitter.com/osborne_plaster" target="_blank"><img src="images/twitter.png"/></a>
+                        </li>
+                        <li><a href="https://www.instagram.com/osborneplastering/" target="_blank"><img src="images/instagram-logo.png"/></a>
+                        </li>
+                    </ul>
+            </div>
+
+            <div class="footer_column" id="footer_column_3">
+                <hr class="mobile_hide">
+                <!--<li>Designer Logo Here</li>-->
+            </div>
+
+            <div class="footer_column" id="footer_column_4">
+                <hr>
+                <li>This site &copy; <?php echo date("Y");?> Osborne Plastering Limited</li>
+            </div>
+        </div>
+        
+        
+        
+    </div>
+<div class="divider2"></div>
